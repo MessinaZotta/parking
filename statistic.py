a11de9ba7c36ac04db516f664314ba2becd46a43
Daniele <you@example.com>
2017-05-30 12:24:43 +0200

    Modified statistic.py

--- statistic.py
+++ statistic.py
@@ -4,14 +4,14 @@ import argparse
 parser = argparse.ArgumentParser()
 
 from datetime import date, timedelta
-yesterday = date.today() - timedelta(1)
+today = date.today()
 
 
 # Opzioni programma (--car, --sensor, --date)
 parser.add_argument("-c", "--car", help="Specify the car you want to see the timing in the parking", type=int, default=None, required=False)
 parser.add_argument("-s", "--sensor", help="Specify the sensor you want to see cars parked", type=int, default=None, required=False)
 parser.add_argument("-d", "--date", help="Specify the date when you want to see all parked cars in that day", type=str, required=False)
-
+#parser.add_argument("-p", "--period", help="Specify the period you want to see arrival/exit of cars", type=str, default=None, nargs='*' required=False)
 
 def main(args):
 
@@ -19,14 +19,14 @@ def main(args):
 
     car = str(args.car) if args.car != None else ""
     sensor = str(args.sensor) if args.sensor != None else ""
-    date = args.date if args.date != None else yesterday.strftime("%Y/%m/%d")
+    date = args.date if args.date != None else today.strftime("%Y/%m/%d")
+
 
     with open("log_saved.txt", "r") as log_file:
         for line in log_file.readlines():
             lcar, lsensor, levent, ldate, ltime = line.replace("\n", "").split(" ")
-            print line.replace("\n", "").split(" ")
-            if car in lcar and sensor in lsensor and date in ldate:
-                print (line.strip())
+            if (car in lcar) and (sensor in lsensor) and (date in ldate):
+                print ("La macchina " + lcar + " è " + ("entrata nel" if levent == "1" else "uscita dal") + " parcheggio " + lsensor + " alle " + ltime + " il " + ldate)
                 nris += 1
 
     log_file.close
