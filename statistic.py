5abb750f290b3cbd720361aa0d8c07467e591c18
Daniele <danielezotta@gmail.com>
2017-05-31 21:57:57 +0200

    Added period check in statistic.py

--- statistic.py
+++ statistic.py
@@ -3,7 +3,7 @@
 import argparse
 parser = argparse.ArgumentParser()
 
-from datetime import date, timedelta
+from datetime import date
 today = date.today()
 
 
@@ -11,7 +11,23 @@ today = date.today()
 parser.add_argument("-c", "--car", help="Specify the car you want to see the timing in the parking", type=int, default=None, required=False)
 parser.add_argument("-s", "--sensor", help="Specify the sensor you want to see cars parked", type=int, default=None, required=False)
 parser.add_argument("-d", "--date", help="Specify the date when you want to see all parked cars in that day", type=str, required=False)
-#parser.add_argument("-p", "--period", help="Specify the period you want to see arrival/exit of cars", type=str, default=None, nargs='*' required=False)
+parser.add_argument("-p", "--period", help="Specify the period you want to see arrival/exit of cars", type=str, default=None, nargs='*', required=False)
+
+
+def checkTime(carTime, time1, time2)):
+    time1 = datetime.datetime.strptime(time1, '%H:%M')
+    time2 = datetime.datetime.strptime(time2, '%H:%M')
+
+    if (time1 > time2):
+        temp = time1
+        time1 = time2
+        time2 = temp
+
+    if (carTime > time1) and (carTime < time2):
+        return True
+    else:
+        return False
+
 
 def main(args):
 
@@ -21,13 +37,23 @@ def main(args):
     sensor = str(args.sensor) if args.sensor != None else ""
     date = args.date if args.date != None else today.strftime("%Y/%m/%d")
 
-
     with open("log_saved.txt", "r") as log_file:
         for line in log_file.readlines():
             lcar, lsensor, levent, ldate, ltime = line.replace("\n", "").split(" ")
             if (car in lcar) and (sensor in lsensor) and (date in ldate):
-                print ("La macchina " + lcar + " è " + ("entrata nel" if levent == "1" else "uscita dal") + " parcheggio " + lsensor + " alle " + ltime + " il " + ldate)
-                nris += 1
+                if (args.period != None):
+                    time1 = args.period[0]
+                    time2 = args.period[1] if args.period[1] != else datetime.datetime.strftime(datetime.datetime.today(), "%H:%M")
+                    if (checkTime(time1, time2)):
+                        print ("La macchina " + lcar + " è " +
+                            ("entrata nel" if levent == "1" else "uscita dal")
+                            + " parcheggio " + lsensor + " alle " + ltime + " il " + ldate)
+                        nris += 1
+                else:
+                    print ("La macchina " + lcar + " è " +
+                        ("entrata nel" if levent == "1" else "uscita dal")
+                        + " parcheggio " + lsensor + " alle " + ltime + " il " + ldate)
+                    nris += 1
 
     log_file.close
 
